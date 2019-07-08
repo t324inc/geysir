88ed4a82b661c50303323af2e181bff23f2e770a
Brian Nowell <briannowell@gmail.com>
2019-07-07 20:27:11 -0700

    Sorting paragraph type options correctly by weight from field settings

--- src/Form/GeysirModalParagraphAddSelectTypeForm.php
+++ src/Form/GeysirModalParagraphAddSelectTypeForm.php
@@ -38,22 +38,49 @@ class GeysirModalParagraphAddSelectTypeForm extends FormBase {
       ->load($parent_entity_type . '.' . $parent_entity_bundle . '.' . $form_mode)
       ->getComponent($field);
 
+    $bundles = $this->getAllowedBundles($bundles);
+
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions($parent_entity_type, $parent_entity_bundle);
+    $field_definition = $bundle_fields[$field];
+    $settings = $field_definition->getSetting('handler_settings');
+    if(!empty($settings['target_bundles'])) {
+      if(!empty($settings['target_bundles_drag_drop'])) {
+        $new_bundles = [];
+        foreach($settings['target_bundles_drag_drop'] as $key => $target_bundle) {
+          if(isset($bundles[$key])) {
+            $new_bundles[] = [
+              'name' => $key,
+              'label' => $bundles[$key],
+              'weight' => $target_bundle['weight'],
+            ];
+          }
+        }
+        $bundles = $new_bundles;
+      }
+    }
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
 
-    $bundles = $this->getAllowedBundles($bundles);
     $paragraphs_type_storage = \Drupal::entityTypeManager()->getStorage('paragraphs_type');
 
     $default_icon = drupal_get_path('module', 'geysir') . '/images/geysir-puzzle.svg';
 
-    foreach ($bundles as $bundle => $label) {
-      $icon_url = $default_icon;
-      if ($paragraphs_type_storage->load($bundle)->getIconUrl()) {
-        $icon_url = $paragraphs_type_storage->load($bundle)->getIconUrl();
+    foreach ($bundles as $key => $bundle) {
+      if(isset($bundle['weight'])) {
+        $label = $bundle['label'];
+        $bundle_name = $bundle['name'];
+      } else {
+        $label = $bundle;
+        $bundle_name = $key;
       }
-      $routeParams['bundle'] = $bundle;
-      $form['description'][$bundle] = [
+      $icon_url = $default_icon;
+      if ($paragraphs_type_storage->load($bundle_name)->getIconUrl()) {
+        $icon_url = $paragraphs_type_storage->load($bundle_name)->getIconUrl();
+      }
+      $routeParams['bundle'] = $bundle_name;
+      $form['description'][$bundle_name] = [
         '#type' => 'image_button',
         '#prefix' => '<div class="geysir-add-type">',
         '#suffix' => '<span>' . $label . '</span></div>',
@@ -80,6 +107,7 @@ class GeysirModalParagraphAddSelectTypeForm extends FormBase {
    */
   protected function getAllowedBundles($allowed_bundles) {
     $bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo('paragraph');
+
 
     if (is_array($allowed_bundles) && count($allowed_bundles)) {
       // Only keep allowed bundles.
